c50a3f448f187f6f50ffa99f8dc9b9671cb38243
Renan Campos Torres <renanctorres7@gmail.com>
2025-03-14 14:23:24 -0300

    Create a widget test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,1 +1,42 @@
-void main() {}
+import 'package:base_project/src/modules/payments/presentation/bloc/bloc.dart';
+import 'package:base_project/src/modules/payments/presentation/page/payments.dart';
+import 'package:bloc_test/bloc_test.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockPaymentsBloc extends MockBloc<PaymentsEvent, PaymentsState>
+    implements PaymentsBloc {}
+
+class MockPaymentsStateFake extends Fake implements PaymentsState {}
+
+class MockPaymentsEventFake extends Fake implements PaymentsEvent {}
+
+void main() {
+  setUp(() {
+    registerFallbackValue(MockPaymentsStateFake());
+    registerFallbackValue(MockPaymentsEventFake());
+  });
+
+  testWidgets('Should render PaymentsTransactionsPage', (tester) async {
+    final mockPaymentsBloc = MockPaymentsBloc();
+
+    when(() => mockPaymentsBloc.state).thenReturn(PaymentsInitial());
+    await tester.pumpWidget(
+      MaterialApp(
+        home: BlocProvider<PaymentsBloc>.value(
+          value: mockPaymentsBloc,
+          child: PaymentsTransactionsPage(),
+        ),
+      ),
+    );
+
+    await tester.pumpAndSettle();
+
+    expect(find.byType(SummaryList), findsOneWidget);
+    expect(find.byType(MakePaymentButton), findsOneWidget);
+    expect(find.byType(ScheduleTransactionsTabBar), findsOneWidget);
+    expect(find.byType(ScheduleList), findsOneWidget);
+  });
+}
